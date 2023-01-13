46fc740120ec2a8469d8183de93ebb19648b1253
Zeshan khan <76880686+zkhan-z@users.noreply.github.com>
2023-01-13 17:47:36 -0500

    Update create_partition_andlv.sh

--- create_partition_andlv.sh
+++ create_partition_andlv.sh
@@ -4,14 +4,21 @@ partition_setup() {
     local disk="$1"
     local mount_point="$2"
     local uuid
+    if [ "$(sudo blkid -s TYPE -o value "${disk}p1")" = "ext4" ]; then
+        echo "Partition already exists, exiting."
+        return 0
+    fi
     sudo gdisk "$disk" <<EOF
-        o
-        Y
-        n
-        1
-        8300
-        w
-        Y
+o
+Y
+n
+1
+      
+
+
+8300
+w
+Y
 EOF
     sudo mkfs.ext4 "${disk}p1"
     if [ ! -d "$mount_point" ]; then
@@ -20,6 +27,7 @@ EOF
     sudo mount "${disk}p1" "$mount_point"
     uuid=$(sudo blkid -s UUID -o value "${disk}p1")
     echo "UUID=$uuid    $mount_point    ext4    defaults    0    0" | sudo tee -a /etc/fstab
+    sudo sed -i '$!N; /^\(.*\)\n\1$/!P; D' /etc/fstab
 }
 
 partition_setup "/dev/nvme1n1" "/var"
